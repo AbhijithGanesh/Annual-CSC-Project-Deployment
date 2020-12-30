af129e38c54618d89f8579db3201f8262ae31a27
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2020-12-30 21:54:40 +0530

    Update wsgi.py

--- Django_Annual/wsgi.py
+++ Django_Annual/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 import sys
+from Django_Annual.wsgi import Password_Generator  as application
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
