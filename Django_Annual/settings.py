54ed1c65423ac73f348cc6317a1d261447a1e93f
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2021-02-27 16:32:23 +0530

    Update settings.py

--- Django_Annual/settings.py
+++ Django_Annual/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 WHITENOISE_USE_FINDERS = True
 ALLOWED_HOSTS = ['*']
