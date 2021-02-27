4239a03a5dcbbc034ae002e4ba0b5fcc30fcdbac
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2021-02-27 16:57:20 +0530

    Update settings.py

--- Django_Annual/settings.py
+++ Django_Annual/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
+DEBUG = (bool,True)
 
 
 WHITENOISE_USE_FINDERS = True
