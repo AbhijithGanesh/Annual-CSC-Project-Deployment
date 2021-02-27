c8f2c4f77f1b722ad5966bf1bc0ca8d4631d601c
AbhijithGanesh <g.abhijith14122003@gmail.com>
2021-02-27 20:26:35 +0530

    Procfile

--- Django_Annual/settings.py
+++ Django_Annual/settings.py
@@ -100,9 +100,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -111,10 +108,7 @@ STATICFILES_DIRS = (
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
