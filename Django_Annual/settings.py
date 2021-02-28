539ae2c1b86080e82e20041b07bac22f66995bcd
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2021-02-28 18:12:11 +0530

    Update settings.py

--- Django_Annual/settings.py
+++ Django_Annual/settings.py
@@ -102,20 +102,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-                    os.path.join(CURRENT_PATH, 'Django_Annual/static'),
-)
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
